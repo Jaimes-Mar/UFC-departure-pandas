ad2e992a9bcaba84df56a88a631a668187006f70
Jaimes-Mar <115047642+Jaimes-Mar@users.noreply.github.com>
2022-10-22 00:37:24 -0500

    Add files via upload

---
+++ UFCroster.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import matplotlib as mp
+
+df= pd.concat(pd.read_excel("UFC Roster Departures.xlsx", sheet_name=None),ignore_index=True)#Combines all the sheets into one dateframe
+
+df.rename(columns = { 'Unnamed: 2':'Reason'}, inplace = True)#gives a name to unnamed column
+
+df.drop(labels=[0],axis=0,inplace=True) #removes empty first row
+
+df2=df.fillna("Reason N/a")# creates 2nd data frame with NaN cells as "Reason N/a"
+
+'''
+SHIT DONT WORK BECAUSE THE DATA FRAME HAS MISSING VALUES FROM COMBINIGNG THE SPREADSHEETS
+df2['Date'] = pd.to_datetime(df2['Date']).dt.normalize()
+
+
+print(df2)
+'''
